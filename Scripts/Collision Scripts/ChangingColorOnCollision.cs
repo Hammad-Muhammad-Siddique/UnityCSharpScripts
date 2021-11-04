35160d0c8219f92c56258d05f6051a3bfadb3792
Hammad Muhammad Siddique <74074430+Hammad-Muhammad-Siddique@users.noreply.github.com>
2021-11-04 05:19:02 +0500

    Add files via upload

---
+++ Scripts/Collision Scripts/ChangingColorOnCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingColorOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        // the renderer component of the gameobject is accessed. It's color is then changed to red
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
