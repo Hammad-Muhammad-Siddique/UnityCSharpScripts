0a302f0c748821287d6ffe4e532f570650daf054
Hammad Muhammad Siddique <74074430+Hammad-Muhammad-Siddique@users.noreply.github.com>
2021-11-04 06:56:47 +0500

    Add files via upload

---
+++ Scripts/Audio Scripts/PlayingAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingAudio : MonoBehaviour
+{
+    public AudioClip Sound; // assign an audio to this vairable in the Unity Editor
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // play the sound audio
+        GetComponent<AudioSource>().PlayOneShot(Sound);
+    }
+}
