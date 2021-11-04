2f4dd18a55c53e9df2008c728e34813bbcdceb20
Hammad Muhammad Siddique <74074430+Hammad-Muhammad-Siddique@users.noreply.github.com>
2021-11-04 07:00:17 +0500

    Add files via upload

---
+++ Scripts/Collectibles Scripts/RotatingCoins.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCoins : MonoBehaviour
+{
+    public float rotateSpeed; // the speed of rotation
+
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, rotateSpeed); // rotating the coin in the upward direction
+    }
+}
