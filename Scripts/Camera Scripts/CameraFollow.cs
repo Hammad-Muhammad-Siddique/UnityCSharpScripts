ae6481e4e45a19c0bfcd44778d4b2cf8867fe090
Hammad Muhammad Siddique <74074430+Hammad-Muhammad-Siddique@users.noreply.github.com>
2021-11-04 06:57:19 +0500

    Add files via upload

---
+++ Scripts/Camera Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // declare all variables
+    public Transform target; // the gameobject at which the camera will look
+
+    // Update is called once per frame
+    void Update()
+    {
+        // look at the target
+        transform.LookAt(target);
+    }
+}
